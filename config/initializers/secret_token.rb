6811c5694ea883768c2c9c0f9e95bcb41866eb72
sangarji <suryadithya@gmail.com>
2012-12-18 09:29:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weddingbells::Application.config.secret_token = 'c5c358ce946b30060caa2d61e493edbea15a94b59e77ae619504b26b18f29dfae131f7d3fc1c3d39fa509963512f56528409aaacad646d4f75bc9448f5cddbff'
